a9b3f20befee0ae6e94315fd819855f618d103c0
Thor Jensen <thorhuga1@live.dk>
2022-08-24 16:33:42 +0200

    remove test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,10 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
 
-  // Go to http://localhost/
-  await page.goto('http://localhost/');
-
-  // Click text=Azure Container instance is UP
-  await page.locator('text=Azure Container instance is UP').click();
 
 });
